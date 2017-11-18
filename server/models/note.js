84dcf936db77089da3924da3ee7cfb6b70ed1300
fgrzelak <fgrzelak@gmail.com>
2017-11-18 13:27:41 +0100

    DELETE NOTE bugfix

--- server/models/note.js
+++ server/models/note.js
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
+import Lane from '../models/lane';
+
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
   task: { type: 'String', required: true },
   id: { type: 'String', required: true, unique: true }
+});
+
+noteSchema.pre('remove', function (next) {
+  Lane.findOneAndUpdate({ notes: { $in: [this._id] } }, { $pull: { notes: this._id } }, next);
 });
 
 export default mongoose.model('Note', noteSchema);
